6fa1375420a335771cb4f3d8add65078fc408d7d
reggie <reggie@latte.to>
2025-02-12 00:33:59 -0600

    Add hearts container for heart icons so text can overlay

--- assets/valentines/hearts.js
+++ assets/valentines/hearts.js
@@ -1,11 +1,14 @@
+const hearts = document.getElementById("hearts");
+
 function createHeart() {
     const heart = document.createElement("img");
     heart.src = "assets/valentines/heart.svg";
-    heart.id = "heart"
+    heart.id = "heart";
     heart.style.scale = Math.random() * 0.5 + 1;
     heart.style.left = Math.random() * 100 + "vw";
     heart.style.animationDuration = Math.random() * 5 + 3 + "s ";
-    document.body.appendChild(heart);
+
+    hearts.appendChild(heart);
     setTimeout(() => {
         heart.remove();
     }, 7000);
