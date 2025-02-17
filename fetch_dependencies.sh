7b920092093bde3003c6311c4ff713d38c285a7a
Justus <>
2025-02-16 23:19:19 -0600

    Revert valentine's changes

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -5,7 +5,7 @@ cd "${0%/*}"
 libraries=(
     "https://unpkg.com/lucide@latest/dist/umd/lucide.min.js"
     "https://unpkg.com/bulma@latest/css/bulma.min.css"
-    "https://unpkg.com/three@0.149.0/build/three.min.js" #three.js became complicated after r160, deprecation warning added in r150. might implement later
+    "https://unpkg.com/three@0.149.0/build/three.min.js"
     "https://unpkg.com/vanta@latest/dist/vanta.dots.min.js"
 )
 
